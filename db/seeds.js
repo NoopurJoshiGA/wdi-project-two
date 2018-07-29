0ded41b40a57e99f19ea285b7aada5183e418564
NoopurJoshiGA <noopur.joshi13@gmail.com>
2018-07-29 15:46:20 +0100

    added 3 column image layout

--- db/seeds.js
+++ db/seeds.js
@@ -44,7 +44,53 @@ Image
         { name: 'Rob', content: 'Wow! This is ❤️!!'}
       ],
       likes: 1000
+    },
+    {
+      image: 'https://images.pexels.com/photos/371393/pexels-photo-371393.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      location: 'Japan',
+      description: 'Sakura Season',
+      comments: [
+        { name: 'Rob', content: 'Wow! This is ❤️!!'}
+      ],
+      likes: 1000
+    },
+    {
+      image: 'https://images.pexels.com/photos/371393/pexels-photo-371393.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      location: 'Japan',
+      description: 'Sakura Season',
+      comments: [
+        { name: 'Rob', content: 'Wow! This is ❤️!!'}
+      ],
+      likes: 1000
+    },
+    {
+      image: 'https://images.pexels.com/photos/371393/pexels-photo-371393.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      location: 'Japan',
+      description: 'Sakura Season',
+      comments: [
+        { name: 'Rob', content: 'Wow! This is ❤️!!'}
+      ],
+      likes: 1000
+    },
+    {
+      image: 'https://images.pexels.com/photos/371393/pexels-photo-371393.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      location: 'Japan',
+      description: 'Sakura Season',
+      comments: [
+        { name: 'Rob', content: 'Wow! This is ❤️!!'}
+      ],
+      likes: 1000
+    },
+    {
+      image: 'https://images.pexels.com/photos/371393/pexels-photo-371393.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      location: 'Japan',
+      description: 'Sakura Season',
+      comments: [
+        { name: 'Rob', content: 'Wow! This is ❤️!!'}
+      ],
+      likes: 1000
     }
+
   ])
   .then(images => console.log(`Created ${images.length} images`))
   .catch(err => console.log(err))
